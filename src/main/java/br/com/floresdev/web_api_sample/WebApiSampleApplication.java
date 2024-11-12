0979e47c58b813b388be56e16121d70e706521ea
Miguel Fernando Flores <miguelfernandoaurelius@gmail.com>
2024-11-12 20:19:16 -0300

    Initial commit

---
+++ src/main/java/br/com/floresdev/web_api_sample/WebApiSampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.floresdev.web_api_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApiSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApiSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/floresdev/web_api_sample/WebApiSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.floresdev.web_api_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApiSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
