dfad5dc4230c8f5d586588d57467d1786c027b6c
Miguel Fernando Flores <miguelfernandoaurelius@gmail.com>
2024-11-12 22:43:16 -0300

    feat(controllers): add WelcomeController to greet users on root path

---
+++ src/main/java/br/com/floresdev/web_api_sample/controllers/WelcomeController.java
@@ -0,0 +1,12 @@
+package br.com.floresdev.web_api_sample.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping
+    public String welcome() {
+        return "Welcome to my Web API Sample!";
+    }
+}
